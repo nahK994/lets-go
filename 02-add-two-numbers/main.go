20fffdd0395444bb472687e5e0c1894e7324957b
nahK994 <nkskl6@gmail.com>
2025-01-10 20:01:31 +0600

    modify 02

--- 02-add-two-numbers/main.go
+++ 02-add-two-numbers/main.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var a int = 7
-	var b int = 5
+	var a int
+	var b int
+	fmt.Printf("Hello! Please give me 2 numbers. I will tell you their summation\n")
+	fmt.Printf("First number: ")
+	fmt.Scanf("%d", &a)
+	fmt.Printf("Second number: ")
+	fmt.Scanf("%d", &b)
 	var c int = a + b
 
-	fmt.Printf("%d+%d=%d\n", a, b, c)
+	fmt.Printf("%d + %d = %d\n", a, b, c)
 }
