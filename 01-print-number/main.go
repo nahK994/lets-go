f2fbfe52be4ffdfed5c1d40fbc33bec4e6a9bc89
nahK994 <nkskl6@gmail.com>
2025-01-18 22:34:44 +0600

    redo 01

--- 01-print-number/main.go
+++ 01-print-number/main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var a int = 2
-	var b int = 5
-	var c int = a * b
+	fmt.Printf("Give me a number; ")
+	var num int
+	fmt.Scanf("%d", &num)
+	var num2 int
+	num2 = num * 2
+	fmt.Printf("double of %d is %d\n", num, num2)
 
-	fmt.Printf("%d x %d = %d\n", a, b, c)
 }
