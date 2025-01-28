2763a0dd7e28a2c5bdafb9b07e0f405cd63a3e66
nahK994 <nkskl6@gmail.com>
2025-01-29 01:20:04 +0600

    update conditional-statements

--- haha.go
+++ haha.go
@@ -3,8 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	var name string
-	fmt.Print("Tell me your name: ")
-	fmt.Scan(&name)
-	fmt.Print("Hello, ", name, "\n")
+	var year int
+	var isLeapYear bool
+	fmt.Scan(&year)
+	if year%4 == 0 {
+		if year%100 == 0 {
+			if year%400 != 0 {
+				isLeapYear = true
+			} else {
+				isLeapYear = false
+			}
+		} else {
+			isLeapYear = true
+		}
+	} else {
+		isLeapYear = false
+	}
+
+	if isLeapYear {
+		fmt.Print(year, " is a leap year\n")
+	} else {
+		fmt.Print(year, " isn't a leap year\n")
+	}
 }
